6ba8aab1354867d38304546565f350a265abdcd4
caiofariaas <caiofarias077@gmail.com>
2024-04-06 09:43:07 -0300

    feat: create table remedios w/ flyway

---
+++ Curso/src/main/resources/db/migration/V1__create-table-remedios.sql
@@ -0,0 +1,11 @@
+ CREATE TABLE remedios(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    via VARCHAR(100) NOT NULL,
+    lote VARCHAR(100) NOT NULL,
+    quantidade VARCHAR(100) NOT NULL,
+    validade VARCHAR(100) NOT NULL,
+    laboratorio VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id)
+ );
