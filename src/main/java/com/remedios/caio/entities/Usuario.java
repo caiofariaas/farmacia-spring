f783f21e7e09101e31eb3248cfadd5a4cce4b1f1
caiofariaas <caiofarias077@gmail.com>
2024-04-17 11:07:39 -0300

    feat: create user entity

---
+++ src/main/java/com/remedios/caio/entities/Usuario.java
@@ -0,0 +1,19 @@
+package com.remedios.caio.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "Usuarios")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
---
+++ src/main/resources/db/migration/V3__create-table-usuarios.sql
@@ -0,0 +1,7 @@
+ CREATE TABLE usuarios(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    login VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(id)
+ );
